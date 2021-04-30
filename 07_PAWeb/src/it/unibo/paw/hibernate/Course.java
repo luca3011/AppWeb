bf75f11fa082c25517141451801dbe9e97f0d903
Luca Tacinelli <lucatacinelli@gmail.com>
2021-04-30 12:09:14 +0200

    completati mapping e javabean es7

---
+++ 07_PAWeb/src/it/unibo/paw/hibernate/Course.java
@@ -0,0 +1,66 @@
+package it.unibo.paw.hibernate;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Course implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String name;
+	
+	//---------------------
+	
+	public Course() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+}
